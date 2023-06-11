b49f49f46ccc0e0a6a6dfb664fffa41601e0294a
douglas godois dos santos <doug_potter92@hotmail.com>
2023-06-11 18:42:17 -0300

    atualizando

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -39,8 +39,8 @@ const trocarCores = ()=>{
 
 /*  função para que ao clicar no botao de baixar meu curriculo a pessoa consiga efetua-lo  */
 
-let url ='https://www.linkedin.com/ambry/?x-li-ambry-ep=AQJVBAooumH8wwAAAYirrw-o0R7px4dmwQ6hhsDStiHvtE42Y02HOPPqIbS5vdQo0Drd39SgKM9xiMeCXM9-0HCEj2y4ZZg8Gct3zgqHhOgXfTklNkBmOcKaJPiJdrpqExXxNO1smEKxF2LUCac1TtmMB9oxCyxEig82h2_vSlqnV88I7XqpAH5QEdwY9d06w7JstdRSX8yyrY6mp6xJWSEzpCWIaKYntcC5xVXfi8dkgitRTCh63f6ZG-EpWQl-oXxH3ZcMpcLepJn_4Co4PbBwCm6ccjvmh5q47A-CFRXxmfyWwXEj-fn07CsvDxFHdjy2Vfxre95HFl8_Rrc5DuKLN7-JVv7czHxjtGcgEmUgLnR2mjIzmKalTulAR8MXeU2fK6KKpkVj6PUTAFgyDnvlYzPNA_SCZjf6k8KXwRB2xCrERlqHWIrbc6m1p8MwZJwg-dU2-NcIS33RD3mkWxQHMXuiy_Me6rqY_dWNyP5XLI1-v3DyKLl7n3ZMVVAdp_8Bk-jygKvhA68CtyGFrBfgAxzkWGiC8iHoDy7uOI8RySLQts-AqB0HeB69YO_DY3NUDg';
-let nomeDoArquivo ='Linkedin+Douglas+Godois+dos++Santos';
+let url ='./assets/IMG/curriculo.pdf';
+let nomeDoArquivo ='Curriculo';
 
 const baixarCurriculo = (url, nomeDoArquivo)=>{
     
